1139424c7faed5e44710d1b7a7b8396a91fd6723
Jon Wee Joey Liew <joeyjon@protonmail.com>
2019-09-13 22:01:57 +0200

    add websocket jwt authentication

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 import "./Chat.css";
 
 const Chat = props => {
+  const [_, token] = localStorage.getItem("jwtToken").split(" ");
+  const URI = encodeURI(`ws://localhost:5000/?token=${token}`);
+  const ws = new WebSocket(URI);
+
   return (
     <div className="chat">
       <div className="chat_rooms">Chat Rooms</div>
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const url = require("url");
 const users = require("./routes/users");
+const keys = require("./config/keys");
 
 const app = express();
 const server = http.createServer(app);
@@ -16,7 +19,21 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 // WebSocket Server
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({
+  server,
+  verifyClient: (info, done) => {
+    let query = url.parse(info.req.url, true).query;
+    jwt.verify(query.token, keys.secretOrKey, (err, decoded) => {
+      if (err) return done(false, 403, "Not valid token");
+      done(true);
+    });
+  }
+});
+
+wss.on("connection", ws => {
+  // get decoded JWT here?
+  console.log("User connected");
+});
 
 // Routes
 app.use("/api/users", users);
